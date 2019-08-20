a2ec888d151320f8c2b5ee9be10eafb29a45b8ab
Andrii Rapavyi <andrapavyy@ukr.net>
2019-08-20 15:52:28 +0300

    new commit

---
+++ Epam Task II/EPAM Task 4/Figure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPAM_Task_4
+{
+    public class Figure:IDrawable
+    {
+        private readonly int X,Y;
+
+        public Figure (int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public virtual void Draw()
+        {
+            Console.WriteLine("Figure " + X + " " + Y);
+        }
+    }
+}
---
+++ Epam Task II/EPAM Task 4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EPAM_Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Square squere = new Square();
+            Rectangle rect = new Rectangle();
+            Figure figure = new Figure(7, 12);
+            squere.Draw();
+            rect.Draw();
+            figure.Draw();
+            Console.WriteLine("Press any key to ESC");
+            Console.ReadKey();
+        }
+    }
+}
